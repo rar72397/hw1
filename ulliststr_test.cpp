9bd446ad610c513557953aeaedc5907809f878a3
Russell Rodriguez <rar72397@usc.edu>
2022-10-03 21:13:26 -0700

    Update ulliststr_test.cpp

--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,11 +1,5 @@
 /* Write your test code for the ULListStr in this file */
-#include <iostream>
-using namespace std;
-
-
-int main(int argc, char* argv[])
-{
-#include "ulliststr.h"
+#nclude "ulliststr.h"
 #include <iostream>
 using namespace std;
 
